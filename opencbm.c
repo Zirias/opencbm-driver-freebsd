c9ef15feeaca8085b7175e0b42eb06eeed80f318
Felix Palmen <felix@palmen-it.de>
2018-11-04 19:53:01 +0100

    driver works

--- opencbm.c
+++ opencbm.c
@@ -22,11 +22,16 @@
 #include <sys/kernel.h>
 #include <sys/types.h>
 #include <sys/systm.h>
+#include <sys/syslog.h>
 
 #include <sys/conf.h>
 #include <sys/uio.h>
 #include <sys/bus.h>
 #include <sys/malloc.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+#include <sys/proc.h>
+#include <sys/condvar.h>
 
 #include <machine/bus.h>
 #include <sys/rman.h>
@@ -36,6 +41,38 @@
 #include "ppbus_if.h"
 #include <dev/ppbus/ppbio.h>
 
+#define CBMCTRL_BASE        0xcb
+
+/* all values needed by PARBURST_READ_TRACK and PARBURST_WRITE_TRACK */
+typedef struct PARBURST_RW_VALUE {
+	unsigned char *buffer;
+	int length;
+} PARBURST_RW_VALUE;
+
+#define CBMCTRL_TALK        _IOW(CBMCTRL_BASE, 0, int)
+#define CBMCTRL_LISTEN      _IOW(CBMCTRL_BASE, 1, int)
+#define CBMCTRL_UNTALK      _IO(CBMCTRL_BASE, 2)
+#define CBMCTRL_UNLISTEN    _IO(CBMCTRL_BASE, 3)
+#define CBMCTRL_OPEN        _IOW(CBMCTRL_BASE, 4, int)
+#define CBMCTRL_CLOSE       _IOW(CBMCTRL_BASE, 5, int)
+#define CBMCTRL_RESET       _IO(CBMCTRL_BASE, 6)
+#define CBMCTRL_GET_EOI     _IOR(CBMCTRL_BASE, 7, int)
+#define CBMCTRL_CLEAR_EOI   _IO(CBMCTRL_BASE, 8)
+
+#define CBMCTRL_PP_READ     _IOR(CBMCTRL_BASE, 10, unsigned char)
+#define CBMCTRL_PP_WRITE    _IOW(CBMCTRL_BASE, 11, unsigned char)
+#define CBMCTRL_IEC_POLL    _IOR(CBMCTRL_BASE, 12, int)
+#define CBMCTRL_IEC_SET     _IOW(CBMCTRL_BASE, 13, int)
+#define CBMCTRL_IEC_RELEASE _IOW(CBMCTRL_BASE, 14, int)
+#define CBMCTRL_IEC_WAIT    _IOWR(CBMCTRL_BASE, 15, int)
+#define CBMCTRL_IEC_SETRELEASE _IOW(CBMCTRL_BASE, 16, int)
+
+#define CBMCTRL_PARBURST_READ    _IOR(CBMCTRL_BASE, 17, unsigned char)
+#define CBMCTRL_PARBURST_WRITE   _IOW(CBMCTRL_BASE, 18, unsigned char)
+#define CBMCTRL_PARBURST_READ_TRACK        _IOW(CBMCTRL_BASE, 19, PARBURST_RW_VALUE)
+#define CBMCTRL_PARBURST_WRITE_TRACK _IOW(CBMCTRL_BASE, 20, PARBURST_RW_VALUE)
+#define CBMCTRL_PARBURST_READ_TRACK_VAR    _IOW(CBMCTRL_BASE, 21, PARBURST_RW_VALUE)
+
 #define CBM_NAME "cbm"
 
 #define IEC_DATA   1
@@ -44,10 +81,12 @@
 #define IEC_RESET  8
 
 /* lpt output lines */
-#define ATN_OUT    0x01
-#define CLK_OUT    0x02
+#define ATN_OUT   0x01
+#define CLK_OUT   0x02
 #define DATA_OUT  0x04
 #define RESET     0x08
+#define LP_IRQ	  0x10
+#define LP_BIDIR  0x20
 
 /* lpt input lines */
 #define ATN_IN     0x10
@@ -69,12 +108,10 @@
 #define CTRL_READ()      (ppb_rctr(ppbus))
 #define CTRL_WRITE(c)    (ppb_wctr(ppbus,c))
 
-#define set_data_forward() do { CTRL_WRITE(CTRL_READ() & 0xdf); \
+#define set_data_forward() do { RELEASE(LP_BIDIR); \
                                  sc->sc_data_reverse = 0; } while (0)
-#define set_data_reverse() do { CTRL_WRITE(CTRL_READ() | 0x20); \
+#define set_data_reverse() do { SET(LP_BIDIR); \
                                  sc->sc_data_reverse = 1; } while (0)
-#define disable_irq() CTRL_WRITE(CTRL_READ() & 0xef)
-#define enable_irq() CTRL_WRITE(CTRL_READ() | 0x10)
 #define timeout_us(us) pause(CBM_NAME, hz/1000000 * us)
 
 static d_open_t cbm_open;
@@ -84,6 +121,8 @@ static d_write_t cbm_write;
 static d_ioctl_t cbm_ioctl;
 
 static int lp = 0;
+
+#define BUFFER_SIZE 256
 
 struct cbm_data {
     int sc_irq_rid;
@@ -99,6 +138,10 @@ struct cbm_data {
     int sc_busy;
     int sc_data_reverse;
     volatile int sc_cbm_irq_count;
+    volatile int sc_eoi;
+    struct mtx sc_lock;
+    struct cv sc_cvp;
+    char sc_buf[BUFFER_SIZE];
 };
 
 static struct cdevsw cbm_cdevsw = {
@@ -167,12 +210,39 @@ wait_for_free_bus(struct cbm_data *sc, device_t ppbus)
     }
 }
 
-static void do_reset(struct cbm_data *sc, device_t ppbus)
+static int
+send_byte(struct cbm_data *sc, device_t ppbus, int b)
+{
+	int i, ack = 0;
+	register_t saveintr;
+
+	log(LOG_DEBUG, "send_byte %02x\n", b);
+
+	saveintr = intr_disable();
+	for (i = 0; i < 8; i++) {
+		DELAY(70);
+		if (!((b >> i) & 1))
+			SET(DATA_OUT);
+		RELEASE(CLK_OUT);
+		DELAY(20);
+		SET_RELEASE(CLK_OUT, DATA_OUT);
+	}
+	intr_restore(saveintr);
+
+	for (i = 0; (i < 20) && !(ack = GET(DATA_IN)); i++)
+		DELAY(100);
+
+	log(LOG_DEBUG, "ack=%d\n", ack);
+
+	return ack;
+}
+
+static void
+do_reset(struct cbm_data *sc, device_t ppbus)
 {
     device_printf(sc->sc_device, "resetting devices\n");
-    RELEASE(DATA_OUT | ATN_OUT | CLK_OUT);
-    set_data_forward();
-    disable_irq();
+    RELEASE(DATA_OUT | ATN_OUT | CLK_OUT | LP_IRQ | LP_BIDIR);
+    sc->sc_data_reverse = 0;
     SET(RESET);
     pause(CBM_NAME, hz/10);
     RELEASE(RESET);
@@ -189,8 +259,6 @@ cbm_open(struct cdev *dev, int oflags __unused, int devtype __unused,
 
     if (sc->sc_busy) return EBUSY;
 
-    // TODO: waitq
-
     sc->sc_busy = 1;
     if (sc->sc_hold_clk) SET(CLK_OUT);
 
@@ -205,24 +273,220 @@ cbm_close(struct cdev *dev, int fflag __unused, int devtype __unused,
     device_t ppbus = device_get_parent(sc->sc_device);
 
     if (!sc->sc_hold_clk) RELEASE(CLK_OUT);
+    sc->sc_busy = 0;
     return 0;
 }
 
 static int
-cbm_write(struct cdev *dev, struct uio *uio, int ioflag)
+wait_for_listener(struct cbm_data *sc, device_t ppbus)
 {
-    (void)dev;
-    (void)uio;
-    (void)ioflag;
-    return 0;
+    mtx_lock(&sc->sc_lock);
+    SET(LP_IRQ);
+    int rv = 0;
+    RELEASE(CLK_OUT);
+    ppb_unlock(ppbus);
+    while (sc->sc_cbm_irq_count &&
+	    !(rv = cv_wait_sig(&sc->sc_cvp, &sc->sc_lock)));
+    ppb_lock(ppbus);
+    RELEASE(LP_IRQ);
+    mtx_unlock(&sc->sc_lock);
+    return rv ? -1 : 0;
 }
 
 static int
-cbm_read(struct cdev *dev, struct uio *uio, int ioflag)
+cbm_raw_write(struct cbm_data *sc, device_t ppbus,
+	const char *buf, size_t cnt, int atn, int talk)
 {
-    (void)dev;
-    (void)uio;
-    (void)ioflag;
+	unsigned char c;
+	int i;
+	int rv = 0;
+	size_t sent = 0;
+	register_t saveintr;
+
+	sc->sc_eoi = sc->sc_cbm_irq_count = 0;
+
+	log(LOG_DEBUG, "cbm_write: %zu bytes, atn=%d\n", cnt, atn);
+
+	RELEASE(DATA_OUT);
+	SET(CLK_OUT | (atn ? ATN_OUT : 0));
+
+	for (i = 0; (i < 100) && !GET(DATA_IN); i++)
+		DELAY(10);
+
+	if (!GET(DATA_IN)) {
+		device_printf(sc->sc_device, "cbm_write: no devices found\n");
+		RELEASE(CLK_OUT | ATN_OUT);
+		return -ENODEV;
+	}
+
+	tsleep(sc, PCATCH, CBM_NAME, hz/50); /* 20ms */
+
+	while (cnt > sent && rv == 0) {
+		c = *buf++;
+		DELAY(50);
+		log(LOG_DEBUG, "cbm_write: trying to write %02x\n", c);
+		if (GET(DATA_IN)) {
+			sc->sc_cbm_irq_count = ((sent == (cnt - 1))
+					 && (atn == 0)) ? 2 : 1;
+
+			if (wait_for_listener(sc, ppbus) < 0) {
+				rv = -EINTR;
+			} else {
+				if (send_byte(sc, ppbus, c)) {
+					sent++;
+					DELAY(100);
+				} else {
+					device_printf(sc->sc_device,
+						"cbm_write: I/O error\n");
+					rv = -EIO;
+				}
+			}
+		} else {
+			device_printf(sc->sc_device,
+				"cbm_write: device not present\n");
+			rv = -ENODEV;
+		}
+	}
+	log(LOG_DEBUG, "%zu bytes sent, rv=%d\n", sent, rv);
+
+	if (talk && (rv == 0)) {
+		saveintr = intr_disable();
+		SET(DATA_OUT);
+		RELEASE(ATN_OUT);
+
+		RELEASE(CLK_OUT);
+		for (i = 0; (i < 100) && !GET(CLK_IN); i++)
+			DELAY(10);
+		if (!GET(CLK_IN)) {
+			device_printf(sc->sc_device,
+				"cbm_write: device not present\n");
+			rv = -ENODEV;
+		}
+
+		intr_restore(saveintr);
+	} else {
+		RELEASE(ATN_OUT);
+	}
+	DELAY(100);
+
+	return (rv < 0) ? rv : (int)sent;
+}
+
+static int
+cbm_write(struct cdev *dev, struct uio *uio, int ioflag __unused)
+{
+    struct cbm_data *sc = dev->si_drv1;
+    device_t ppbus = device_get_parent(sc->sc_device);
+    int rv = 0;
+
+    ppb_lock(ppbus);
+    while (uio->uio_resid > 0)
+    {
+	int amount = MIN(uio->uio_resid, BUFFER_SIZE);
+	rv = uiomove(sc->sc_buf, amount, uio);
+	if (rv != 0) break;
+	rv = cbm_raw_write(sc, ppbus, sc->sc_buf, amount, 0, 0);
+	if (rv < 0)
+	{
+	    rv = -rv;
+	    break;
+	}
+	rv = 0;
+    }
+    ppb_unlock(ppbus);
+    return rv;
+}
+
+static int
+cbm_read(struct cdev *dev, struct uio *uio, int ioflag __unused)
+{
+    size_t received = 0;
+    int bufpos = 0;
+    int i, b, bit;
+    int ok = 0;
+    register_t saveintr;
+
+    struct cbm_data *sc = dev->si_drv1;
+    device_t ppbus = device_get_parent(sc->sc_device);
+
+    ssize_t count = uio->uio_resid;
+    log(LOG_DEBUG, "cbm_read: %zd bytes\n", count);
+
+    if (sc->sc_eoi) return 0;
+
+    ppb_lock(ppbus);
+    do {
+	i = 0;
+	while (GET(CLK_IN)) {
+	    if (i >= 50) {
+		if (tsleep(dev, PCATCH, CBM_NAME, hz/50) != EWOULDBLOCK)
+		{
+		    ppb_unlock(ppbus);
+		    return EINTR;
+		}
+	    } else {
+		i++;
+		DELAY(20);
+	    }
+	}
+	saveintr = intr_disable();
+	RELEASE(DATA_OUT);
+	for (i = 0; (i < 40) && !(ok = GET(CLK_IN)); i++)
+	    DELAY(10);
+	if (!ok) {
+	    sc->sc_eoi = 1;
+	    SET(DATA_OUT);
+	    DELAY(70);
+	    RELEASE(DATA_OUT);
+	}
+	for (i = 0; i < 100 && !(ok = GET(CLK_IN)); i++)
+	    DELAY(20);
+	for (bit = b = 0; (bit < 8) && ok; bit++) {
+	    for (i = 0; (i < 200) && !(ok = (GET(CLK_IN) == 0)); i++)
+		DELAY(10);
+	    if (ok) {
+		b >>= 1;
+		if (GET(DATA_IN) == 0)
+		    b |= 0x80;
+		for (i = 0; i < 100 && !(ok = GET(CLK_IN)); i++)
+		    DELAY(20);
+	    }
+	}
+	if (ok)
+	    SET(DATA_OUT);
+	intr_restore(saveintr);
+	if (ok) {
+	    ++received;
+	    sc->sc_buf[bufpos++] = b;
+	    if (bufpos == BUFFER_SIZE)
+	    {
+		log(LOG_DEBUG, "cbm_read: moving whole buffer\n");
+		uiomove(sc->sc_buf, BUFFER_SIZE, uio);
+		bufpos = 0;
+	    }
+	    if (received % 256)
+		DELAY(50);
+	    else
+		tsleep(dev, PCATCH, CBM_NAME, hz/50);
+	}
+    } while (received < count && ok && !sc->sc_eoi);
+    ppb_unlock(ppbus);
+
+    if (!ok)
+    {
+	device_printf(sc->sc_device, "cbm_read: I/O error\n");
+	return EIO;
+    }
+
+    if (bufpos)
+    {
+	log(LOG_DEBUG, "cbm_read: moving %d bytes\n", bufpos);
+	uiomove(sc->sc_buf, bufpos, uio);
+    }
+
+    log(LOG_DEBUG, "received=%zu, count=%zd, ok=%d, eoi=%d\n",
+	    received, count, ok, sc->sc_eoi);
+
     return 0;
 }
 
@@ -230,17 +494,279 @@ static int
 cbm_ioctl(struct cdev *dev, u_long cmd, caddr_t data, int fflag,
 	struct thread *td)
 {
-    (void)dev;
-    (void)cmd;
-    (void)data;
-    (void)fflag;
-    (void)td;
-    return 0;
+	struct cbm_data *sc = dev->si_drv1;
+	device_t ppbus = device_get_parent(sc->sc_device);
+
+	unsigned char buf[2], c, talk, mask, state, i;
+	int rv = 0;
+	int arg = 0;
+	PARBURST_RW_VALUE *val = 0;
+
+	if (cmd == CBMCTRL_TALK
+		|| cmd == CBMCTRL_LISTEN
+		|| cmd == CBMCTRL_OPEN
+		|| cmd == CBMCTRL_CLOSE
+		|| cmd == CBMCTRL_IEC_SET
+		|| cmd == CBMCTRL_IEC_RELEASE
+		|| cmd == CBMCTRL_IEC_WAIT
+		|| cmd == CBMCTRL_IEC_SETRELEASE)
+	    arg = *(int *)data;
+
+	buf[0] = (arg >> 8) & 0x1f;	/* device */
+	buf[1] = arg & 0x0f;	/* secondary address */
+
+	ppb_lock(ppbus);
+	switch (cmd) {
+	case CBMCTRL_RESET:
+		do_reset(sc, ppbus);
+		rv = 0;
+		break;
+
+	case CBMCTRL_TALK:
+	case CBMCTRL_LISTEN:
+		talk = (cmd == CBMCTRL_TALK);
+		buf[0] |= talk ? 0x40 : 0x20;
+		buf[1] |= 0x60;
+		rv = cbm_raw_write(sc, ppbus, buf, 2, 1, talk);
+		rv = rv > 0 ? 0 : -rv;
+		break;
+
+	case CBMCTRL_UNTALK:
+	case CBMCTRL_UNLISTEN:
+		buf[0] = (cmd == CBMCTRL_UNTALK) ? 0x5f : 0x3f;
+		rv = cbm_raw_write(sc, ppbus, buf, 1, 1, 0);
+		rv = rv > 0 ? 0 : -rv;
+		break;
+
+	case CBMCTRL_OPEN:
+	case CBMCTRL_CLOSE:
+		buf[0] |= 0x20;
+		buf[1] |= (cmd == CBMCTRL_OPEN) ? 0xf0 : 0xe0;
+		rv = cbm_raw_write(sc, ppbus, buf, 2, 1, 0);
+		if ((cmd == CBMCTRL_CLOSE) && (rv == 0)) {
+			/* issue an unlisten */
+			buf[0] = 0x3f;
+			cbm_raw_write(sc, ppbus, buf, 1, 1, 0);
+		}
+		rv = rv > 0 ? 0 : -rv;
+		break;
+
+	case CBMCTRL_GET_EOI:
+		*(int *)data = sc->sc_eoi ? 1 : 0;
+		rv = 0;
+		break;
+
+	case CBMCTRL_CLEAR_EOI:
+		sc->sc_eoi = 0;
+		rv = 0;
+		break;
+
+	case CBMCTRL_IEC_WAIT:
+		rv = 0;
+		switch (arg >> 8) {
+		case IEC_DATA:
+			mask = DATA_IN;
+			break;
+		case IEC_CLOCK:
+			mask = CLK_IN;
+			break;
+		case IEC_ATN:
+			mask = ATN_IN;
+			break;
+		default:
+			rv = EINVAL;
+		}
+		if (rv) break;
+		state = (arg & 0xff) ? mask : 0;
+		i = 0;
+		while ((POLL() & mask) == state) {
+			if (i >= 20) {
+				if (tsleep(dev, PCATCH, CBM_NAME, hz/50)
+					!= EWOULDBLOCK)
+				{
+				    rv = EINTR;
+				    break;
+				}
+			} else {
+				i++;
+				DELAY(10);
+			}
+		}
+		if (rv) break;
+		/* fall through */
+
+	case CBMCTRL_IEC_POLL:
+		c = POLL();
+		if ((c & DATA_IN) == 0)
+			rv |= IEC_DATA;
+		if ((c & CLK_IN) == 0)
+			rv |= IEC_CLOCK;
+		if ((c & ATN_IN) == 0)
+			rv |= IEC_ATN;
+		*(int *)data = rv;
+		rv = 0;
+		break;
+
+	case CBMCTRL_IEC_SET:
+		if (arg & ~(IEC_DATA | IEC_CLOCK | IEC_ATN | IEC_RESET)) {
+			/*
+			 * there was some bit set that is not recognized, return
+			 * with an error
+			 */
+			rv = EINVAL;
+		} else {
+			if (arg & IEC_DATA)
+				SET(DATA_OUT);
+			if (arg & IEC_CLOCK)
+				SET(CLK_OUT);
+			if (arg & IEC_ATN)
+				SET(ATN_OUT);
+			if (arg & IEC_RESET)
+				SET(RESET);
+			rv = 0;
+		}
+		break;
+
+	case CBMCTRL_IEC_RELEASE:
+		if (arg & ~(IEC_DATA | IEC_CLOCK | IEC_ATN | IEC_RESET)) {
+			/*
+			 * there was some bit set that is not recognized, return
+			 * with an error
+			 */
+			rv = EINVAL;
+		} else {
+			if (arg & IEC_DATA)
+				RELEASE(DATA_OUT);
+			if (arg & IEC_CLOCK)
+				RELEASE(CLK_OUT);
+			if (arg & IEC_ATN)
+				RELEASE(ATN_OUT);
+			if (arg & IEC_RESET)
+				RELEASE(RESET);
+			rv = 0;
+		}
+		break;
+
+	case CBMCTRL_IEC_SETRELEASE:
+		{
+			unsigned set = arg >> 8;
+			unsigned release = arg & 0xFF;
+			unsigned set_mask = 0;
+			unsigned release_mask = 0;
+
+			if ((set &
+			     ~(IEC_DATA | IEC_CLOCK | IEC_ATN | IEC_RESET))
+			    || (release &
+				~(IEC_DATA | IEC_CLOCK | IEC_ATN | IEC_RESET)))
+			{
+				/*
+				 * there was some bit set that is not recognized, return
+				 * with an error
+				 */
+				rv = EINVAL;
+			} else {
+				if (set & IEC_DATA)
+					set_mask |= DATA_OUT;
+				if (set & IEC_CLOCK)
+					set_mask |= CLK_OUT;
+				if (set & IEC_ATN)
+					set_mask |= ATN_OUT;
+				if (set & IEC_RESET)
+					set_mask |= RESET;
+
+				if (release & IEC_DATA)
+					release_mask |= DATA_OUT;
+				if (release & IEC_CLOCK)
+					release_mask |= CLK_OUT;
+				if (release & IEC_ATN)
+					release_mask |= ATN_OUT;
+				if (release & IEC_RESET)
+					release_mask |= RESET;
+
+				SET_RELEASE(set_mask, release_mask);
+				rv = 0;
+			}
+		}
+		break;
+
+	case CBMCTRL_PP_READ:
+		if (!sc->sc_data_reverse) {
+			XP_WRITE(0xff);
+			set_data_reverse();
+		}
+		*(unsigned char *)data = XP_READ();
+		rv = 0;
+		break;
+
+	case CBMCTRL_PP_WRITE:
+		arg = *(unsigned char *)data;
+		if (sc->sc_data_reverse)
+			set_data_forward();
+		XP_WRITE(arg);
+		rv = 0;
+		break;
+
+/* and now the parallel burst-routines */
+
+	case CBMCTRL_PARBURST_READ:
+		*(unsigned char *)data = cbm_parallel_burst_read(sc, ppbus);
+		rv = 0;
+		break;
+
+	case CBMCTRL_PARBURST_WRITE:
+		arg = *(unsigned char *)data;
+		rv = cbm_parallel_burst_write(sc, ppbus, arg);
+		break;
+
+	case CBMCTRL_PARBURST_READ_TRACK:
+		val = (PARBURST_RW_VALUE *)data;
+		rv = !cbm_parallel_burst_read_track(sc, ppbus,
+			val->buffer);
+		break;
+
+	case CBMCTRL_PARBURST_READ_TRACK_VAR:
+		val = (PARBURST_RW_VALUE *)data;
+		rv = !cbm_parallel_burst_read_track_var(sc, ppbus,
+			val->buffer);
+		break;
+
+	case CBMCTRL_PARBURST_WRITE_TRACK:
+		val = (PARBURST_RW_VALUE *)data;
+		rv = !cbm_parallel_burst_write_track(sc, ppbus,
+			val->buffer, val->length);
+		break;
+
+	default:
+		rv = ENOTTY;
+	}
+	ppb_unlock(ppbus);
+	return rv;
 }
 
 static void
 cbm_intr(void *arg)
 {
+    struct cbm_data *sc = arg;
+    device_t ppbus = device_get_parent(sc->sc_device);
+
+    POLL();
+    if (sc->sc_cbm_irq_count == 0)
+    {
+	log(LOG_DEBUG, "cbm_intr(): spurious interrupt\n");
+	return;
+    }
+    else if (--sc->sc_cbm_irq_count == 0)
+    {
+	ppb_lock(ppbus);
+	SET(CLK_OUT);
+	log(LOG_DEBUG, "cbm_intr(): continue to send\n");
+	ppb_unlock(ppbus);
+	cv_signal(&sc->sc_cvp);
+    }
+    else
+    {
+	log(LOG_DEBUG, "cbm_intr(): must still wait\n");
+    }
 }
 
 static void
@@ -293,7 +819,7 @@ cbm_attach(device_t dev)
     args.mda_devsw = &cbm_cdevsw;
     args.mda_uid = UID_ROOT;
     args.mda_gid = GID_OPERATOR;
-    args.mda_mode = 0600;
+    args.mda_mode = 0660;
     error = make_dev_s(&args, &sc->sc_cdev, CBM_NAME);
     if (error)
     {
@@ -315,6 +841,17 @@ cbm_attach(device_t dev)
 
     device_t ppbus = device_get_parent(dev);
     ppb_lock(ppbus);
+
+    if (!PPB_IN_PS2_MODE(ppbus) && ppb_set_mode(ppbus, PPB_SPP) < 0)
+    {
+	ppb_unlock(ppbus);
+	destroy_dev(sc->sc_cdev);
+	bus_release_resource(dev, SYS_RES_IRQ, sc->sc_irq_rid,
+		sc->sc_irq_resource);
+	device_printf(dev, "unable to set SPP port mode\n");
+	return EINVAL;
+    }
+
     error = ppb_request_bus(ppbus, dev, PPB_WAIT | PPB_INTR);
     if (error)
     {
@@ -352,13 +889,14 @@ cbm_attach(device_t dev)
 
     sc->sc_busy = 0;
 
-    RELEASE(DATA_OUT | ATN_OUT | CLK_OUT);
-    set_data_forward();
-    disable_irq();
-
-    pause(CBM_NAME, hz/20);
-
+    RELEASE(DATA_OUT | ATN_OUT | CLK_OUT | LP_IRQ | LP_BIDIR);
+    sc->sc_data_reverse = 0;
     ppb_unlock(ppbus);
+
+    mtx_init(&sc->sc_lock, CBM_NAME, 0, MTX_DEF);
+    cv_init(&sc->sc_cvp, CBM_NAME);
+
+    tsleep(sc, PCATCH, CBM_NAME, hz/20);
 
     return 0;
 }
@@ -369,6 +907,8 @@ cbm_detach(device_t dev)
     int error = 0;
     struct cbm_data *sc = device_get_softc(dev);
 
+    cv_destroy(&sc->sc_cvp);
+    mtx_destroy(&sc->sc_lock);
     destroy_dev(sc->sc_cdev);
 
     device_t ppbus = device_get_parent(dev);
@@ -528,13 +1068,6 @@ int cbm_handshaked_read(struct cbm_data *sc, device_t ppbus, int toggle)
 	int returnvalue2, returnvalue3, timeoutcount;
 	int to = 0;
 
-	RELEASE(DATA_IN);	/* not really needed? */
-
-	/* linux
-	   RELEASE(DATA_OUT);
-	   DELAY(2);
-	   GET(DATA_IN); */
-
 	if (!toggle) {
 		while (GET(DATA_IN))
 			if (to++ > TO_HANDSHAKED_READ)
@@ -573,8 +1106,6 @@ int cbm_handshaked_write(struct cbm_data *sc, device_t ppbus,
 	char data, int toggle)
 {
 	int to = 0;
-
-	RELEASE(CLK_IN);
 
 	if (!toggle) {
 		while (GET(DATA_IN))
